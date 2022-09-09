d07a6d667b7f792dbb80fd3ab745895de16517a7
Petter Thowsen <petterthowsen@gmail.com>
2022-09-09 21:26:03 +0200

    y

--- Console/Commands/QuickStartCommand.gd
+++ Console/Commands/QuickStartCommand.gd
@@ -5,12 +5,14 @@ const message = """
 Enter "make my_game" to make a new project.
 Enter "open my_game" to open it.
 
+Enter "install_demos" to install the demo projects.
+
 Use CTRL+Q and CTRL+E to navigate between the Console, Spriter, Mapper etc.
 Use CTRL+S to save, CTRL+R to run.
 
 In the Coder window, use CTRL+Tab or CTRL+Space to toggle the scripts list.
 
-To browse & play games, enter "browse".
+To browse & play games, enter "browse" (there probably aren't any games available yet!)
 """
 
 func run(args:Array = []):
